35f96986282fa15e69f8991dce56b1d3e8d83ba5
camilanaomitm <camila.mori1408@gmail.com>
2020-06-02 01:11:01 -0300

    Correções
    Arrumar Câmera

---
+++ Michi - Copia 2/Assets/Scripts/TriggerCutscene.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TriggerCutscene : MonoBehaviour
+{
+    public Image fade;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+
+            //if(Input.GetKeyDown(KeyCode.F))
+            //{
+                StartCoroutine(Fim());
+            //}
+        }
+        
+    }
+
+    IEnumerator Fim()
+    {
+        yield return new WaitForSeconds(0.2f);
+        Color cor = fade.color;
+
+        yield return new WaitForSeconds(1);
+
+        while (cor.a < 1)
+        {
+            cor.a += Time.deltaTime;
+            fade.color = cor;
+            yield return null;
+        }
+
+        SceneManager.LoadScene("Cutscene Final");
+    }
+}
