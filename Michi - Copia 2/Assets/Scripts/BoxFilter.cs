ee43655518419b71b0ee996525daba4043a45c63
debax123 <48129664+debax123@users.noreply.github.com>
2020-06-08 17:21:46 -0300

    08/06
    08/06 até as 18h

---
+++ Michi - Copia 2/Assets/Scripts/BoxFilter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxFilter : MonoBehaviour
+{
+    public Collider col1;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Physics.IgnoreCollision(collision.collider, col1);
+        }
+    }
+}
